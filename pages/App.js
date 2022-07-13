c5271ad1994ee863946d566a4ebdb32a4d23a054
Jhosua Dany <jhosuacontjhosua@gmail.com>
2022-07-13 17:19:10 -0500

    Add the background

--- pages/App.js
+++ pages/App.js
@@ -1,10 +1,10 @@
 import Home from "./pages/Home";
 import Header from "./pages/Header";
-
+import image from "./../pages/static/architecture.jpg";
 
 function App() {
     return (
-        <div className="bg-black">
+        <div className={`bg-[url('./../pages/static/abstract.jpg')]`}>
             <Header />
             <Home />
         </div>
